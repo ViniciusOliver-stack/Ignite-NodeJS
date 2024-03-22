f1d053afbf094efd41ffce66c85cbdd989421a5b
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-03-22 07:40:57 -0300

    feat: conditional for checking which database is used.

--- Curso 01/src/database.ts
+++ Curso 01/src/database.ts
@@ -5,10 +5,13 @@ import { env } from "./env"
 dotenv.config()
 
 export const config = {
-  client: "sqlite",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     directory: "./db/migrations",
