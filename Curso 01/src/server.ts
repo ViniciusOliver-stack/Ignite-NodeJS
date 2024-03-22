9c64930c6e65236949e22516db149bd0efd54f69
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-03-21 21:07:38 -0300

    feat: remove lines and add to app.ts file

--- Curso 01/src/server.ts
+++ Curso 01/src/server.ts
@@ -1,15 +1,5 @@
-import fastify from "fastify"
-import cookie from "@fastify/cookie"
-
 import { env } from "./env"
-import { transactions } from "./routes/transactions"
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactions, {
-  prefix: "transactions",
-})
+import { app } from "./app"
 
 app
   .listen({
